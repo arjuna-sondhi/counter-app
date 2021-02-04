bda5c86e6dabea431b30b5f6fe2e675cc8e08362
Arjuna Sondhi <arjuna.sondhi@gmail.com>
2021-02-03 23:33:23 -0500

    Adding state to a react component
    1. Converted Counter componet from function to class component
    2. Moved increment function within the class
    3. Created contructor for the Counter component to store state
    4. Created state object with counter attribute to store state of counter
    5. Wrote logic for increment function to increment the value of the counter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React, { Component } from "react";
 import FirstComponent from "./components/learning-examples/FirstComponent";
 import SecondComponent from "./components/learning-examples/SecondComponent";
 import ThirdComponent from "./components/learning-examples/ThirdComponent";
-import logo from "./logo.svg";
+import Counter from "./components/counter/Counter";
+// import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Counter />
+      </div>
+    );
+  }
+}
+
+//Leanring Components example code
+class LearningComponents extends Component {
+  render() {
+    return (
+      <div className="learningComponents">
         My Hello World
         <FirstComponent />
         <SecondComponent />
