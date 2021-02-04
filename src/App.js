37ddae9945ee909d0524b5c3fc5701715700d0ab
Arjuna Sondhi <arjuna.sondhi@gmail.com>
2021-02-03 22:55:45 -0500

    Components folder structure
    Put components in their individual files, created folder structure for components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import FirstComponent from "./components/learning-examples/FirstComponent";
+import SecondComponent from "./components/learning-examples/SecondComponent";
+import ThirdComponent from "./components/learning-examples/ThirdComponent";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -10,34 +13,9 @@ class App extends Component {
         <FirstComponent />
         <SecondComponent />
         <ThirdComponent />
-        <FourthComponent />
       </div>
     );
   }
-}
-
-//Class Component
-class FirstComponent extends Component {
-  render() {
-    return <div className="firstComponent">First Component</div>;
-  }
-}
-
-//Class Component
-class SecondComponent extends Component {
-  render() {
-    return <div className="secondComponent">Second Component</div>;
-  }
-}
-
-//Function Component
-function ThirdComponent() {
-  return <div className="thirdComponent">Third Component</div>;
-}
-
-//Function Component
-function FourthComponent() {
-  return <div className="fourthComponent">Fourth Component</div>;
 }
 
 export default App;
---
+++ src/components/learning-examples/FirstComponent.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+//Class Component
+class FirstComponent extends Component {
+  render() {
+    return <div className="firstComponent">First Component</div>;
+  }
+}
+
+//Class Component
+export class SecondComponent extends Component {
+  render() {
+    return <div className="secondComponent">Second Component</div>;
+  }
+}
+
+export default FirstComponent;
